db1d8b2186df22dbafd5a54a086ca94e80dac63a
Antichess <cahalng@gmail.com>
2023-01-31 20:11:24 -0500

    initial find coordiantes

--- src/overpass.py
+++ src/overpass.py
@@ -1,7 +1,7 @@
 import overpy
 api = overpy.Overpass()
 
-quick_c = [43.6367540538917, -79.5086528971227, 43.63242876197476, -79.50096042252115]
+quick_c = [43.87947760607178, -79.49165054311786, 43.866101913596125, -79.46284804882069]
 #quick_c is quick coordinate, paste in two coordinates into the list above and it will be re-ordered for OSM as the bounding box
 if quick_c[2] < quick_c[0]:
     t = quick_c[0]
@@ -15,13 +15,12 @@ if quick_c[3] < quick_c[1]:
 print(quick_c)
 
 query = f"""
-    node({quick_c[0]},{quick_c[1]},{quick_c[2]},{quick_c[3]})["amenity"="school"];
-    out;
+    node({quick_c[0]},{quick_c[1]},{quick_c[2]},{quick_c[3]})["highway"="bus"];out;
     """
 
 print(query)
 result = api.query(query)
-# result = api.query("node(50.745,7.17,50.75,7.18);out;")
+#result = api.query("node(50.745,7.17,50.75,7.18);out;")
 with open("output.txt", "w") as f:
     for x in result.nodes:
         #print(x.__dict__)
