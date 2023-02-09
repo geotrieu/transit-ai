51bc24a970a253ddef12862b36d0075c3b7fbeac
geotrieu <trieugeorge@gmail.com>
2023-02-09 12:18:42 -0500

    Added Leaflet support

---
+++ transit-ui/src/components/LeafletModule.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+import { Marker, Popup, useMap } from "react-leaflet";
+
+import "../styles/LeafletModule.css";
+
+const DEFAULT_ZOOM = 10;
+
+const ChangeView = ({ center, zoom }) => {
+    const map = useMap();
+    map.setView(center, zoom);
+};
+
+const LeafletModule = ({ latitude, longitude, zoom, markers }) => {
+    const zoomLevel = zoom ? zoom : DEFAULT_ZOOM;
+    console.log(markers);
+    return (
+        <MapContainer
+            className="leaflet-map"
+            center={[latitude, longitude]}
+            zoom={zoomLevel}
+            scrollWheelZoom={true}
+        >
+            <ChangeView center={[latitude, longitude]} zoom={zoomLevel} />
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {markers.map((marker) => (
+                <Marker
+                    key={marker.key}
+                    position={[marker.latitude, marker.longitude]}
+                >
+                    <Popup>{marker.text}</Popup>
+                </Marker>
+            ))}
+        </MapContainer>
+    );
+};
+
+export default LeafletModule;
--- transit-ui/src/components/ModelContainer.jsx
+++ transit-ui/src/components/ModelContainer.jsx
@@ -1,15 +1,84 @@
-import React from 'react';
+import React, { useState } from "react";
 
 import "../styles/ModelContainer.css";
+import SelectMenu from "./common/SelectMenu";
+import LeafletModule from "./LeafletModule";
+
+const cityDetails = {
+    YYZ: {
+        name: "Toronto",
+        latitude: 43.65628,
+        longitude: -79.38836,
+    },
+    YVR: {
+        name: "Vancouver",
+        latitude: 49.28012,
+        longitude: -123.12082,
+    },
+    YGK: {
+        name: "Kingston",
+        latitude: 44.22528,
+        longitude: -76.49514,
+    },
+};
+
+const markers = [
+    {
+        key: "YYZ",
+        latitude: 43.65628,
+        longitude: -79.38836,
+        text: "Toronto YYZ Marker",
+    },
+    {
+        key: "YVR",
+        latitude: 49.28012,
+        longitude: -123.12082,
+        text: "Vancouver YVR Marker",
+    },
+    {
+        key: "YGK",
+        latitude: 44.22528,
+        longitude: -76.49514,
+        text: "Kingston YGK Marker",
+    },
+];
 
 const ModelContainer = () => {
+    const cityOptions = Object.entries(cityDetails).map(([key, value]) => ({
+        option: value.name,
+        value: key,
+    }));
+
+    const [city, setCity] = useState(cityOptions.at(0).value);
 
     return (
         <div className="model-container content">
             <h3>Transit AI Predictor</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <span>
+                <SelectMenu
+                    className="model-location-select"
+                    onSelect={setCity}
+                    options={cityOptions}
+                />
+                <h6 className="model-location">Now Showing: {city}</h6>
+            </span>
+            <LeafletModule
+                latitude={cityDetails[city].latitude}
+                longitude={cityDetails[city].longitude}
+                zoom={14}
+                markers={markers}
+            />
         </div>
     );
-}
- 
+};
+
 export default ModelContainer;
