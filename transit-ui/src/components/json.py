a9e9ce4d79f176028a50a479ebfe95812922621f
Antichess <cahalng@gmail.com>
2023-02-16 14:35:47 -0500

    initial json

---
+++ transit-ui/src/components/json.py
@@ -0,0 +1,7 @@
+d = {}
+d["line1"] = []
+d["line1"].append({"name": "Yonge Station", "lat": 43, "long": -79})
+d["line1"].append({"name": "Keele Station", "lat": 42, "long": -79})
+d["line2"] = []
+d["line2"].append({"name": "Bloor Station", "lat": 44, "long": -80})
+print(d)
