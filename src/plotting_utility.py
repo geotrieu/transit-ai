6680e9afbc344a5aee0d3a1d02b5f2cc68634e52
George Trieu <trieugeorge@gmail.com>
2023-03-01 00:31:38 -0500

    Interchanges, Major Refactoring (#26)
    * fixed some merge bugs, and changed bounds
    
    * Added export functions
    
    * Refactored scoring to own file, modularized to support multiple datasets
    
    * ignore pycache
    
    * wip
    
    * Interchanges
    
    * fixed dataset
    
    * Major Refactoring, added clustering notebook
    
    * restored transit-ui:
    
    * Edited Station Generator to conform to refactoring

---
+++ src/plotting_utility.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plotGridPoints2D(gridPoints, color=None):
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    fig.set_figheight(10)
+    fig.set_figwidth(10)
+    ax.scatter(gridPoints["lat"], gridPoints["long"], c=color)
+    return ax
+
+def plotGridPoints3D(gridPoints, color=None):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    fig.set_figheight(10)
+    fig.set_figwidth(10)
+    ax.scatter(gridPoints["lat"], gridPoints["long"], gridPoints["score"], c=color)
+    return ax
+
+def plotGridPoints3DWithLine(gridPoints, line):
+    ax = plotGridPoints3D(gridPoints)
+    ax.plot([line[0], line[1]], [line[2], line[3]], [0, 0], color='red', linewidth=3)
+    return ax
+
+def plotGridPoints3DWithPoints(gridPoints, points):
+    ax = plotGridPoints3D(gridPoints)
+    x = points[0]
+    y = points[1]
+    ax.scatter(x, y, 0)
+    return ax
+
+def plotHeatmap(gridPoints, line, density):
+    y_begin = line[2]
+    y_end = line[3]
+    fig = plt.figure()
+    ax = sns.heatmap(gridPoints.pivot("long", "lat", "score"))
+    y_begin_hm = int((y_begin - gridPoints["long"].iloc[0]) / (gridPoints["long"].iloc[-1] - gridPoints["long"].iloc[0]) * density)
+    y_end_hm = int((y_end - gridPoints["long"].iloc[0]) / (gridPoints["long"].iloc[-1] - gridPoints["long"].iloc[0]) * density)
+    ax.plot([0, density], [y_begin_hm, y_end_hm], linewidth=3, color='r')
+    return ax
+
+def plotHeatmapPoints(gridPoints, points, density):
+    fig = plt.figure()
+    ax = sns.heatmap(gridPoints.pivot("long", "lat", "score"))
+    for p in points:
+        x = p[0]
+        y = p[1]
+        x_hm = [int(a) for a in ((x - gridPoints["lat"].iloc[0]) / (gridPoints["lat"].iloc[-1] - gridPoints["lat"].iloc[0]) * density)]
+        y_hm = [int(a) for a in ((y - gridPoints["long"].iloc[0]) / (gridPoints["long"].iloc[-1] - gridPoints["long"].iloc[0]) * density)]
+        ax.scatter(x_hm, y_hm, linewidth=1)
+    return ax
